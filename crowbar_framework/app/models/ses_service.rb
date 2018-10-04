6650360b074a77ee25456a8e1caf5ca126698291
Walter A. Boring IV <waboring@hemna.com>
2018-10-04 19:03:30 +0000

    Got the SES integration to show up in the ui
    After updating to this, run
    
    systemctl restart crowbar

--- crowbar_framework/app/models/ses_service.rb
+++ crowbar_framework/app/models/ses_service.rb
@@ -18,29 +18,23 @@ class SesService < OpenstackServiceObject
   def initialize(thelogger = nil)
     super(thelogger)
     @bc_name = "ses"
+    @logger = thelogger
   end
 end
 
-  def self.allow_multiple_proposals?
-    false
-  end
 
   class << self
+    # turn off multi proposal support till it really works and people ask for
+    # it.
+    def self.allow_multiple_proposals?
+      false
+    end
+
     def role_constraints
       {
-        "cinder-controller" => {
+        "ses-controller" => {
           "unique" => false,
           "count" => 1,
-          "cluster" => true,
-          "admin" => false,
-          "exclude_platform" => {
-            "suse" => "< 12.3",
-            "windows" => "/.*/"
-          }
-        },
-        "cinder-volume" => {
-          "unique" => false,
-          "count" => -1,
           "cluster" => true,
           "admin" => false,
           "exclude_platform" => {
@@ -54,8 +48,12 @@ end
 
   def proposal_dependencies(role)
     answer = []
-    ["cinder", "keystone", "glance", "nova"].each do |dep|
-      answer << { "barclamp" => dep, "inst" => role.default_attributes[@bc_name]["#{dep}_instance"] }
+    deps = ["cinder", "keystone", "glance", "nova"]
+    deps.each do |dep|
+      answer << {
+        "barclamp" => dep,
+        "inst" => role.default_attributes[@bc_name]["#{dep}_instance"]
+      }
     end
     answer
   end
@@ -65,13 +63,6 @@ end
     base = super
 
     nodes = NodeObject.all
-    controllers = select_nodes_for_role(nodes, "cinder-controller", "controller") || []
-    storage = select_nodes_for_role(nodes, "cinder-volume", "storage") || []
-
-    base["deployment"][@bc_name]["elements"] = {
-      "cinder-controller" => controllers.empty? ? [] : [controllers.first.name],
-      "cinder-volume" => storage.map { |x| x.name }
-    }
 
     base["attributes"][@bc_name]["cinder_instance"] = find_dep_proposal("cinder")
     base["attributes"][@bc_name]["nova_instance"] = find_dep_proposal("nova")
@@ -83,6 +74,7 @@ end
   end
 
   def validate_proposal_after_save(proposal)
+    validate_one_for_role proposal, "ses-controller"
 
     super
   end
@@ -92,4 +84,3 @@ end
 
     @logger.debug("Cinder apply_role_pre_chef_call: leaving")
   end
-end
